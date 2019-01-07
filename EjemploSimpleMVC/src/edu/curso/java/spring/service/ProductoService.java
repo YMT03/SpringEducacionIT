9306fb5e57d037f2aee49eb4608632e20de3a66d
YMT03 <nachito.eze.ramos@gmail.com>
2019-01-07 13:17:26 -0300

    configuracion hibernate adaptada

--- EjemploSimpleMVC/src/edu/curso/java/spring/service/ProductoService.java
+++ EjemploSimpleMVC/src/edu/curso/java/spring/service/ProductoService.java
@@ -1,9 +1,13 @@
 package edu.curso.java.spring.service;
 
+import java.util.List;
+
 import edu.curso.java.spring.model.Producto;
 
 public interface ProductoService {
 	
-	public void addProducto(Producto producto) throws ProductoException;
+	public long addProducto(Producto producto) throws ProductoException;
+	
+	List<Producto> getAll();
 
 }
--- EjemploSimpleMVC/src/edu/curso/java/spring/service/ProductoServiceImp.java
+++ EjemploSimpleMVC/src/edu/curso/java/spring/service/ProductoServiceImp.java
@@ -1,5 +1,7 @@
 package edu.curso.java.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,14 +16,21 @@ public class ProductoServiceImp implements ProductoService{
 	@Autowired
 	private ProductoDAO productoDAO;
 	
-	public void addProducto(Producto producto) throws ProductoException {
+	public long addProducto(Producto producto) throws ProductoException {
 		double precioConIVA = producto.getPrecio() * 1.21;
 		producto.setPrecioConIVA(precioConIVA);
 		productoDAO.add(producto);
 		//Excepcion para probar el transactional.. Debera hacer rollback
 		if(producto.getPrecio()<0)
 		throw new ProductoException("El precio ingresado es incorrecto");
+		return producto.getId();
 	}
+
+	public List<Producto> getAll() {
+		return productoDAO.getAll();
+	}
+	
+	
 
 	
 	
