662395a300c565d5e3e94393c9139391de81bdfd
YMT03 <nachito.eze.ramos@gmail.com>
2019-01-09 13:49:01 -0300

    start de rest

---
+++ EjemploSimpleMVC/src/edu/curso/java/spring/mvc/ProductosRestController.java
@@ -0,0 +1,24 @@
+package edu.curso.java.spring.mvc;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.curso.java.spring.model.dto.ProductoDTO;
+
+@RestController
+@RequestMapping("/api/productos")
+public class ProductosRestController {
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<ProductoDTO> getProductoById(@PathVariable("id") Long id){
+		//TODO -> Hardcoded
+		ProductoDTO productoDTO = new ProductoDTO();
+		productoDTO.setId(id);
+		productoDTO.setNombre("productinho");
+		productoDTO.setPrecio(123.0);
+		return ResponseEntity.ok(productoDTO);
+	}
+}
